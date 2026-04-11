599269587c72464b29ebadd9f808db34b2a75e4d
Matt <mblackman@users.noreply.github.com>
2026-04-11 12:32:33 -0600

    Added unit test support

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testMatch: ['<rootDir>/tests/**/*.test.js'],
+  transform: {
+    '\\.js$': ['babel-jest', { plugins: ['@babel/plugin-transform-modules-commonjs'] }],
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '\\$:/core/modules/utils/diff-match-patch/diff_match_patch\\.js': '<rootDir>/tests/dmp-shim.js',
+  },
+  setupFiles: ['<rootDir>/tests/setup.js'],
+  collectCoverageFrom: [
+    'plugins/revision-history/src/**/*.js',
+  ],
+  coverageDirectory: 'coverage',
+};
